91647b0b0416c5e984249f1c237c49091d561914
Kenta-Miyoshi-work <miyoshi.kenta@dentsusoken.com>
2026-07-16 15:15:37 +0900

    mock全般

--- src/App.jsx
+++
@@ -1,5 +0,0 @@
-import FormBuilder from "@/features/form-builder/FormBuilder";
-
-export default function App() {
-  return <FormBuilder />;
-}
---
+++ src/app/form-builder/page.js
@@ -0,0 +1,7 @@
+"use client";
+
+import FormBuilderClient from "./FormBuilderClient";
+
+export default function FormBuilderPage() {
+  return <FormBuilderClient />;
+}
